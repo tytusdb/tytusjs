a433a588ce606d0c4e3633e21b75e50748390feb
georgeno1ukno <201020588@ingenieria.usac.edu.gt>
2021-07-01 22:52:40 -0600

    adding method constructor  Layerlink

--- src/NeuronalNetwork.js
+++ src/NeuronalNetwork.js
@@ -126,5 +126,28 @@ class Matriz {
 
 
 class LayerLink {
+    constructor(prevNode_count, node_count) {
+        this.weights = new Matrix(node_count, prevNode_count);
+        this.bias = new Matrix(node_count, 1);
+        this.weights.randomize();
+        this.bias.randomize();
+
+        //console.table(this.weights.data)
+        //console.table(this.bias.data)
+    }
+
+    updateWeights(weights) {
+        this.weights = weights;
+    }
+    getWeights() {
+        return this.weights;
+    }
+    getBias() {
+        return this.bias;
+    }
+    add(deltaWeight, bias) {
+        this.weights.add(deltaWeight);
+        this.bias.add(bias);
+    }
 
 }
