024f78f1ee95ee0eb6248a99bc9c0451120dfb00
georgeno1ukno <201020588@ingenieria.usac.edu.gt>
2021-07-01 22:33:47 -0600

    fix static multiply

--- src/NeuronalNetwork.js
+++ src/NeuronalNetwork.js
@@ -20,6 +20,17 @@ class Matriz {
             console.log("Cannot Operate, Check Matrix Multiplication Rules.");
             return undefined;
         } else {
+            let result = new Matrix(m1.rows, m2.cols);
+
+            for (let i = 0; i < result.rows; i++)
+                for (let j = 0; j < result.cols; j++) {
+                    let sum = 0;
+                    for (let k = 0; k < m1.cols; k++) {
+                        sum += m1.data[i][k] * m2.data[k][j];
+                    }
+                    result.data[i][j] = sum;
+                }
+            return result;
 
         }
     }
