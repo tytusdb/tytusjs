04cee8df346ca247f11caec45a37840f407fd0ee
Diego <gomezefdiego@gmail.com>
2021-07-03 01:10:23 -0600

    2D kmeans clustering

--- src/kmeans.js
+++ src/kmeans.js
@@ -170,7 +170,50 @@ class _2DKMeans extends KMeans {
     }
 
     clusterize(k, data, iterations) {
+        for (let i = 0; i < iterations; i++) {
+            let clusters = []
 
+
+            //pendiente agregar iterations
+            //let copy_data = [...this.data]
+            for (let i = 0; i < k; i++) {
+                let c = data[Math.floor(Math.random() * data.length)]
+                while (clusters.findIndex(x => x === c) != -1) {
+                    c = data[Math.floor(Math.random() * data.length)]
+                }
+                clusters.push(c)
+            }
+
+            clusters = clusters.sort(function (a, b) { return (a > b) ? 1 : ((b > a) ? -1 : 0); })
+            //console.log('Clusters: ', clusters)
+
+            //distancias [punto,cluster,distancia]
+            let distances = []
+            let clustered_data = []
+            let closer_cluster = 0, closer_distance = 0, first_distance = true;
+            data.forEach(point => {
+                closer_cluster = 0, closer_distance = 0, first_distance = true;
+
+                clusters.forEach(c => {
+                    let distance = this.distance(point, c)
+                    if (first_distance) {
+                        closer_distance = Math.abs(distance)
+                        closer_cluster = c
+                        first_distance = !first_distance
+                    } else {
+                        if (Math.abs(distance) < closer_distance) {
+                            closer_distance = Math.abs(distance)
+                            closer_cluster = c
+                        }
+
+                    }
+                    distances.push([point, c, distance])
+
+
+                })
+                clustered_data.push([point, closer_cluster, closer_distance])
+            });
+        }
     }
 
     distance(point_a, point_b) {
