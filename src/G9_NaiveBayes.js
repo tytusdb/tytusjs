82ca802f9243b521ecbf12ffb79d600ab7238505
Carlos Peralta <carlospecam@gmail.com>
2021-07-04 23:50:04 -0600

    funcionalidad hashmap

--- src/G9_NaiveBayes.js
+++ src/G9_NaiveBayes.js
@@ -399,10 +399,69 @@ DecisionTree.prototype.predictClass = function(ejemplo){
         if(nodoHijo.length === 0){
         return raiz = 'No se puede clasificar';
         }
-        raiz = nodoHijo[0].valor; // 只遍历attribute节点
+        raiz = nodoHijo[0].valor; // Solo atraviesa el nodo de atributo
     }
     return raiz.vals;
 }
+
+/* Implementacion del hashmap*/
+function HashMap(obj) {
+  this.length = 0;
+  this.obj = new Object();
+}
+
+HashMap.prototype = {
+  isEmpty: function(){
+    return this.length === 0;
+  },
+  hasKey: function(key){
+    return (key in this.obj);
+  },
+  hasVal: function(val){
+    for(var key in this.obj){
+      if(this.obj[key] === value){
+        return true;
+      }
+    }
+    return false;
+  },
+  put: function(key, value){
+    if(!this.hasKey(key)){
+      this.length++;
+    }
+    this.obj[key] = value;
+  },
+  get: function(key){
+    return this.obj[key];
+  },
+  remove: function(key){
+    if(this.hasKey(key) && delete this.obj[key]){
+      this.length--;
+    }
+  },
+  keys: function(){
+    var _keys = new Array();
+    for(var key in this.obj){
+      _keys.push(key);
+    }
+    return _keys;
+  },
+  values: function(){
+    var _vals = new Array();
+    for(var key in this.obj){
+      _vals.push(this.obj[key]);
+    }
+    return _vals;
+  },
+  size: function(){
+    return this.length;
+  },
+  clear: function() {
+    this.length = 0;
+    this.obj = new Object();
+  }
+}
+
 
 //fin c
 
